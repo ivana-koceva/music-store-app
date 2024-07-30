8bc3a999599067fa13e79d9e4c2e88667d5df73f
Ivana Koceva <ivana.koceva10@gmail.com>
2024-07-30 15:12:25 +0200

    refactoring

---
+++ MusicStoreApplication/MusicStore.Service/Implementation/TrackService.cs
@@ -0,0 +1,48 @@
+﻿using MusicStore.Repository.Interface;
+using MusicStore.Service.Interface;
+using MusicStore.Web.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicStore.Service.Implementation
+{
+    public class TrackService : ITrackService
+    {
+        private readonly IRepository<Track> _trackRepository;
+
+        public TrackService(IRepository<Track> trackRepository)
+        {
+            _trackRepository = trackRepository;
+        }
+
+        public void CreateNewTrack(Track p)
+        {
+            _trackRepository.Insert(p);
+        }
+
+        public void DeleteTrack(Guid id)
+        {
+            var track = _trackRepository.Get(id);
+            _trackRepository.Delete(track);
+        }
+
+        public List<Track> GetAllTracks()
+        {
+            return _trackRepository.GetAll().ToList();
+        }
+
+        public Track GetDetailsForTrack(Guid? id)
+        {
+            var track = _trackRepository.Get(id);
+            return track;
+        }
+
+        public void UpdateExistingTrack(Track p)
+        {
+            _trackRepository.Update(p);
+        }
+    }
+}
