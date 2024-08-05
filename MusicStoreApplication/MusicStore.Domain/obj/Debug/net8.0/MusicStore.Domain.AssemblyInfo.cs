209c2a9e142369f1b95aab8350e326946d3dab07
anajaneska <anajaneska1@outlook.com>
2024-08-05 13:29:41 +0200

    .

--- MusicStoreApplication/MusicStore.Domain/obj/Debug/net8.0/MusicStore.Domain.AssemblyInfo.cs
+++ MusicStoreApplication/MusicStore.Domain/obj/Debug/net8.0/MusicStore.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicStore.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80a84e2edb66191884508c20c569b3819c48de44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e299cb4756dab66aab8c93336fd290d62e51f785")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicStore.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicStore.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
